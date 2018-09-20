623d6226d8e28f3c8c022d7b413b99c4d27b9094
Jesse Loftus <jesse.loftus@gmail.com>
2018-09-19 18:23:51 -0700

    got result to properly be defined before the db create.

--- models/Job.js
+++ models/Job.js
@@ -1,11 +1,11 @@
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
 
 // Save a reference to the Schema constructor
 var Schema = mongoose.Schema;
 
 // Using the Schema constructor, create a new UserSchema object
 // This is similar to a Sequelize model
-var ArticleSchema = new Schema({
+var JobSchema = new Schema({
   // `title` is required and of type String
   title: {
     type: String,
@@ -18,15 +18,15 @@ var ArticleSchema = new Schema({
   },
   // `note` is an object that stores a Note id
   // The ref property links the ObjectId to the Note model
-  // This allows us to populate the Article with an associated Note
+  // This allows us to populate the Job with an associated Note
   note: {
     type: Schema.Types.ObjectId,
-    ref: "Note"
+    ref: 'Note'
   }
 });
 
 // This creates our model from the above schema, using mongoose's model method
-var Article = mongoose.model("Article", ArticleSchema);
+var Job = mongoose.model('Job', JobSchema);
 
-// Export the Article model
-module.exports = Article;
+// Export the Job model
+module.exports = Job;
--- server.js
+++ server.js
@@ -22,7 +22,9 @@ var app = express();
 // Use morgan logger for logging requests
 app.use(logger('dev'));
 // Use body-parser for handling form submissions
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static('public'));
 
@@ -32,93 +34,116 @@ mongoose.connect('mongodb://localhost/job_scraper_db');
 // Routes
 
 // A GET route for scraping the echoJS website
-app.get('/scrape', function(req, res) {
+app.get('/scrape', function (req, res) {
 	// First, we grab the body of the html with request
-	axios.get('https://news.ycombinator.com/jobs').then(function(response) {
+	axios.get('https://news.ycombinator.com/jobs').then(function (response) {
 		// Then, we load that into cheerio and save it to $ for a shorthand selector
 		var $ = cheerio.load(response.data);
 
-		// Now, we grab every td and do the following:
-		$('td').each(function(i, element) {
+		// grab every td and do the following:
+		$('td').each((index, item) => {
+			// console.log('#####', $(this))
 			// Save an empty result object
+			$item = $(item)
+			console.log('item  ',item)
+			console.log('this  ',$(this))
 			var result = {};
-
+			result.link = $item
+				.children('a')
+				.attr('href')
 			// Add the text and href of every link, and save them as properties of the result object
-			result.title = $(this)
+			result.title = $item
 				.children('a')
 				.text();
-			result.link = $(this)
-				.children('a')
-				.attr('href');
 
-			// Create a new Article using the `result` object built from scraping
-			db.Article.create(result)
-				.then(function(dbArticle) {
-					// View the added result in the console
-					console.log(dbArticle);
-				})
-				.catch(function(err) {
-					// If an error occurred, send it to the client
-					return res.json(err);
-				});
+			// Create a new Job using the `result` object built from scraping
+			if (result.title && result.link && result.link.includes('http')) {
+				createJob(result)
+			}
 		});
 
-		// If we were able to successfully scrape and save an Article, send a message to the client
+		// If we were able to successfully scrape and save an Job, send a message to the client
 		res.send('Scrape Complete');
 	});
 });
 
-// Route for getting all Articles from the db
-app.get('/articles', function(req, res) {
-	// Grab every document in the Articles collection
-	db.Article.find({})
-		.then(function(dbArticle) {
-			// If we were able to successfully find Articles, send them back to the client
-			res.json(dbArticle);
+// Route for getting all Jobs from the db
+app.get('/Jobs', function (req, res) {
+	// Grab every document in the Jobs collection
+	db.Job.find({})
+		.then(function (dbJob) {
+			// If we were able to successfully find Jobs, send them back to the client
+			res.json(dbJob);
 		})
-		.catch(function(err) {
+		.catch(function (err) {
 			// If an error occurred, send it to the client
 			res.json(err);
 		});
 });
 
-// Route for grabbing a specific Article by id, populate it with it's note
-app.get('/articles/:id', function(req, res) {
+// Route for grabbing a specific Job by id, populate it with it's note
+app.get('/Jobs/:id', function (req, res) {
 	// Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-	db.Article.findOne({ _id: req.params.id })
-	// ..and populate all of the notes associated with it
-		.populate('note')
-		.then(function(dbArticle) {
-			// If we were able to successfully find an Article with the given id, send it back to the client
-			res.json(dbArticle);
+	db.Job.findOne({
+			_id: req.params.id
 		})
-		.catch(function(err) {
+		// ..and populate all of the notes associated with it
+		.populate('note')
+		.then(function (dbJob) {
+			// If we were able to successfully find an Job with the given id, send it back to the client
+			res.json(dbJob);
+		})
+		.catch(function (err) {
 			// If an error occurred, send it to the client
 			res.json(err);
 		});
 });
 
-// Route for saving/updating an Article's associated Note
-app.post('/articles/:id', function(req, res) {
+// Route for saving/updating an Job's associated Note
+app.post('/Jobs/:id', function (req, res) {
 	// Create a new note and pass the req.body to the entry
 	db.Note.create(req.body)
-		.then(function(dbNote) {
-			// If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+		.then(function (dbNote) {
+			// If a Note was created successfully, find one Job with an `_id` equal to `req.params.id`. Update the Job to be associated with the new Note
 			// { new: true } tells the query that we want it to return the updated User -- it returns the original by default
 			// Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-			return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+			return db.Job.findOneAndUpdate({
+				_id: req.params.id
+			}, {
+				note: dbNote._id
+			}, {
+				new: true
+			});
 		})
-		.then(function(dbArticle) {
-			// If we were able to successfully update an Article, send it back to the client
-			res.json(dbArticle);
+		.then(function (dbJob) {
+			// If we were able to successfully update an Job, send it back to the client
+			res.json(dbJob);
 		})
-		.catch(function(err) {
+		.catch(function (err) {
 			// If an error occurred, send it to the client
 			res.json(err);
 		});
 });
 
 // Start the server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
 	console.log('App running on port ' + PORT + '!');
 });
+
+
+function createJob(result) {
+
+	db.Job.create(result)
+
+		.then(function (dbJob) {
+
+			// View the added result in the console
+			console.log('dbjob',dbJob);
+			return dbJob;
+		})
+		.catch(function (err) {
+			// If an error occurred, send it to the client
+			console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',err);
+			throw new Error(err);
+		})
+}
